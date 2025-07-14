39dcf874d8e2966d15a4507aaebbb80c8be6979c
doris <dorishsh2678@gmail.com>
2025-07-14 14:03:47 +0800

    註冊帳號

---
+++ api/db.php
@@ -0,0 +1,175 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function q($sql){
+    $dsn='mysql:host=localhost;dbname=db13;charset=utf8';
+    $pdo=new PDO($dsn,'root','');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function to($url){
+    header("location:".$url);
+}
+
+class DB{
+private $dsn="mysql:host=localhost;dbname=db13;charset=utf8";
+private $pdo;   
+private $table;
+
+function __construct($table){
+    $this->table=$table;
+    $this->pdo=new PDO($this->dsn,"root",'');
+}
+
+function all(...$arg){
+    $sql="select * from $this->table ";
+    if(isset($arg[0])){
+        if(is_array($arg[0])){
+            $tmp=$this->arraytosql($arg[0]);
+            $sql=$sql." where ".join(" AND " , $tmp);
+
+        }else{
+            $sql .= $arg[0];
+        }
+    }
+
+    if(isset($arg[1])){
+        $sql .= $arg[1];
+    }
+
+    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function count(...$arg){
+    $sql="select count(*) from $this->table ";
+    if(isset($arg[0])){
+        if(is_array($arg[0])){
+            $tmp=$this->arraytosql($arg[0]);
+            $sql=$sql." where ".join(" AND " , $tmp);
+
+        }else{
+            $sql .= $arg[0];
+        }
+    }
+
+    if(isset($arg[1])){
+        $sql .= $arg[1];
+    }
+
+    return $this->pdo->query($sql)->fetchColumn();
+}
+
+function sum($col,...$arg){
+    $sql="select sum($col) from $this->table ";
+    if(isset($arg[0])){
+        if(is_array($arg[0])){
+            $tmp=$this->arraytosql($arg[0]);
+            $sql=$sql." where ".join(" AND " , $tmp);
+
+        }else{
+            $sql .= $arg[0];
+        }
+    }
+
+    if(isset($arg[1])){
+        $sql .= $arg[1];
+    }
+
+    return $this->pdo->query($sql)->fetchColumn();
+}
+
+function find($id){
+    $sql="select * from $this->table ";
+    
+    if(is_array($id)){
+        $tmp=$this->arraytosql($id);
+        $sql=$sql." where ".join(" AND " , $tmp);
+
+    }else{
+        $sql .= " WHERE `id`='$id'";
+    }
+    //echo $sql;
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+}
+
+function save($array){
+    if(isset($array['id'])){
+        //update
+        $sql="update $this->table set ";
+        $tmp=$this->arraytosql($array);
+        $sql.= join(" , ",$tmp) . "where `id`= '{$array['id']}'";
+    }else{
+        //insert
+        $cols=join("`,`",array_keys($array));
+        $values=join("','",$array);
+        $sql="insert into $this->table (`$cols`) values('$values')";
+    }
+    //echo $sql;
+    return $this->pdo->exec($sql);
+}
+
+function del($id){
+    $sql="delete  from $this->table ";
+    
+    if(is_array($id)){
+        $tmp=$this->arraytosql($id);
+        $sql=$sql." where ".join(" AND " , $tmp);
+
+    }else{
+        $sql .= " WHERE `id`='$id'";
+    }
+    //echo $sql;
+    return $this->pdo->exec($sql);
+}
+
+
+private function arraytosql($array){
+    $tmp=[];
+    foreach($array as $key => $value){
+        $tmp[]="`$key`='$value'";
+    }
+
+    return $tmp;
+}
+
+}
+
+
+//  $User->save(['acc'=>'test','pw'=>'5678','email'=>'test@labor.gov.tw']);
+//  $User->save(['acc'=>'mem01','pw'=>'mem01','email'=>'mem01@labor.gov.tw']);
+//  $User->save(['acc'=>'mem02','pw'=>'mem02','email'=>'mem02@labor.gov.tw']);
+
+
+// $Title=new DB('title');
+// $Ad=new DB('ad');
+// $Mvim=new DB('mvim');
+// $Image=new DB('image');
+// $News=new DB('news');
+// $Admin=new DB('admin');
+// $Menu=new DB('menu');
+// $Total=new DB('total');
+// $Bottom=new DB('bottom');
+
+ $User=new DB('user');
+ $Visit=new DB('visit');
+
+if(!isset($_SESSION['visit'])){
+    //第一次來訪
+    $today=$Visit->find(['date'=>date("Y-m-d")]);
+    if(empty($today)){
+       $Visit->save(['date'=>date("Y-m-d"),'visit'=>1]);
+    }else{
+       $today['visit']++; 
+       $Visit->save($today);
+    }
+   $_SESSION['visit']=1;
+}
+
+?>
---
+++ front/forgot.php
@@ -0,0 +1,10 @@
+<fieldset>
+   <legend>忘記密碼</legend>
+   <div>請入信箱以查詢密碼</div>
+   <div>
+      <input type="tel" name="email" id="email">
+   </div>
+   <div>
+      <button>尋找</button>
+   </div>
+</fieldset>
